705b9f0a7c1b865bac1f1bec01c84a35afd98698
apaixonadopormotores <pedrofernandes0124@gmail.com>
2024-03-26 09:03:30 -0300

    joia

---
+++ jogodopou/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace jogodopou;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ jogodopou/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace jogodopou;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
