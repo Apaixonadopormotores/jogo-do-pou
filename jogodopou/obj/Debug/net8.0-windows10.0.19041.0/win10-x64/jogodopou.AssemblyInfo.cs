bcb120bcd01a1a97105c410e00325cc344eefc0a
apaixonadopormotores <pedrofernandes0124@gmail.com>
2024-04-11 11:57:58 -0300

    jhgjhgjhgjh

--- jogodopou/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/jogodopou.AssemblyInfo.cs
+++ jogodopou/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/jogodopou.AssemblyInfo.cs
@@ -10,10 +10,14 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.jogodopou")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "jogodopou")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("jogodopou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7149da3165fdac7ca110d04887b841d168e7326")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a7149da3165fdac7ca110d04887b841d168e7326")]
 [assembly: System.Reflection.AssemblyProductAttribute("jogodopou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jogodopou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
